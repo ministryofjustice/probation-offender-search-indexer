e73183c05345dfb35503f83031c2fbd0287018d5
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-07-07 14:53:03 +0100

    DT-961 circle localstack config

--- src/test/kotlin/uk/gov/justice/digital/hmpps/indexer/ProbationOffenderSearchIndexerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/indexer/ProbationOffenderSearchIndexerTest.kt
@@ -2,12 +2,12 @@ package uk.gov.justice.digital.hmpps.indexer
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles(profiles = ["test"])
 class ProbationOffenderSearchIndexerTest {
   @Test
-  fun test() {
-
-  }
+  fun `The application starts`() {}
 }
 
