80eba8db2b28bcce474510cf80c43d1510c0e71d
Andy Marke <andy.marke@digital.justice.gov.uk>
2020-07-17 09:18:31 +0100

    DT-961 Recreate index when asked

--- src/test/kotlin/uk/gov/justice/digital/hmpps/indexer/service/OffenderSynchroniserServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/indexer/service/OffenderSynchroniserServiceTest.kt
@@ -4,10 +4,12 @@ import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.check
 import com.nhaarman.mockitokotlin2.isA
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import uk.gov.justice.digital.hmpps.indexer.model.IndexStatus
 import uk.gov.justice.digital.hmpps.indexer.model.SyncIndex.GREEN
@@ -19,36 +21,83 @@ internal class OffenderSynchroniserServiceTest {
   private val indexStatusService = mock<IndexStatusService>()
   private val service = OffenderSynchroniserService(communityApi, offenderRepository, indexStatusService)
 
-  @BeforeEach
-  internal fun setUp() {
-    whenever(communityApi.getOffender(any())).thenReturn(anOffender())
-    whenever(indexStatusService.getIndexStatus()).thenReturn(IndexStatus.newIndex())
+  @Nested
+  inner class SynchroniseOffender {
+
+    @BeforeEach
+    internal fun setUp() {
+      whenever(communityApi.getOffender(any())).thenReturn(anOffender())
+      whenever(indexStatusService.getIndexStatus()).thenReturn(IndexStatus.newIndex())
+    }
+
+    @Test
+    fun `should retrieve offender`() {
+      service.synchroniseOffender("X12345")
+
+      verify(communityApi).getOffender("X12345")
+    }
+
+    @Test
+    internal fun `will save offender to repository`() {
+      service.synchroniseOffender("X12345")
+
+      verify(offenderRepository).save(isA(), isA())
+    }
+
+    @Test
+    internal fun `will save offender to current index`() {
+      val index = IndexStatus.newIndex().toBuildComplete()
+      assertThat(index.currentIndex).isEqualTo(GREEN)
+
+      whenever(indexStatusService.getIndexStatus()).thenReturn(index)
+
+      service.synchroniseOffender("X12345")
+
+      verify(offenderRepository).save(isA(), check { assertThat(it).isEqualTo(GREEN) })
+    }
   }
 
-  @Test
-  fun `should retrieve offender`() {
-    service.synchroniseOffender("X12345")
+  @Nested
+  inner class CheckExistsAndReset {
+    @Nested
+    inner class IndexExists {
+      @BeforeEach
+      internal fun setUp() {
+        whenever(offenderRepository.doesIndexExist(GREEN)).thenReturn(true)
 
-    verify(communityApi).getOffender("X12345")
-  }
+        service.checkExistsAndReset(GREEN)
+      }
+      @Test
+      internal fun `will delete the index if it exists`() {
+        verify(offenderRepository).deleteIndex(GREEN)
+      }
+      @Test
+      internal fun `will recreate the index`() {
+        verify(offenderRepository).createIndex(GREEN)
+      }
+    }
+    @Nested
+    inner class IndexDoesNotExists {
+      @BeforeEach
+      internal fun setUp() {
+        whenever(offenderRepository.doesIndexExist(GREEN)).thenReturn(false)
 
-  @Test
-  internal fun `will save offender to repository`() {
-    service.synchroniseOffender("X12345")
+        service.checkExistsAndReset(GREEN)
+      }
+      @Test
+      internal fun `won't bother deleting index if it does not exist`() {
+        whenever(offenderRepository.doesIndexExist(GREEN)).thenReturn(false)
 
-    verify(offenderRepository).save(isA(), isA())
-  }
+        service.checkExistsAndReset(GREEN)
 
-  @Test
-  internal fun `will save offender to current index`() {
-    val index = IndexStatus.newIndex().toBuildComplete()
-    assertThat(index.currentIndex).isEqualTo(GREEN)
+        verify(offenderRepository, never()).deleteIndex(any())
+      }
+      @Test
+      internal fun `will create the index`() {
+        verify(offenderRepository).createIndex(GREEN)
+      }
 
-    whenever(indexStatusService.getIndexStatus()).thenReturn(index)
-
-    service.synchroniseOffender("X12345")
-
-    verify(offenderRepository).save(isA(), check { assertThat(it).isEqualTo(GREEN) })
+    }
   }
 }
 
