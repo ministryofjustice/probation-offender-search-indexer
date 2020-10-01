cb4f7ae880e9cc099e24984b26f01a3533552e58
Mike Halma <mike.halma@digital.justice.gov.uk>
2020-10-01 12:14:46 +0100

    DT-1167 Use latest versions (mitigates CVE-2020-5421)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-	id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.2"
+	id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.0.6"
 	kotlin("plugin.spring") version "1.4.0"
 }
 
@@ -21,23 +21,23 @@ dependencies {
 
 	implementation("org.springframework:spring-jms")
 	implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-	implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.854")
+	implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.873")
 
 	implementation("com.google.code.gson:gson:2.8.6")
-	implementation("io.arrow-kt:arrow-core:0.10.5")
+	implementation("io.arrow-kt:arrow-core:0.11.0")
 
-	implementation("org.springdoc:springdoc-openapi-ui:1.4.6")
-	implementation("org.springdoc:springdoc-openapi-data-rest:1.4.6")
-	implementation("org.springdoc:springdoc-openapi-kotlin:1.4.6")
+	implementation("org.springdoc:springdoc-openapi-ui:1.4.8")
+	implementation("org.springdoc:springdoc-openapi-data-rest:1.4.8")
+	implementation("org.springdoc:springdoc-openapi-kotlin:1.4.8")
 
-	testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.1")
+	testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 	testImplementation("org.testcontainers:localstack:1.14.3")
 	testImplementation("org.testcontainers:elasticsearch:1.14.3")
 	testImplementation("org.awaitility:awaitility-kotlin:4.0.3")
-	testImplementation("io.kotest:kotest-assertions-arrow-jvm:4.2.3")
+	testImplementation("io.kotest:kotest-assertions-arrow-jvm:4.2.5")
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.18.1")
+	testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.19.0")
 	testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.21")
-	testImplementation("org.mockito:mockito-inline:3.5.9")
+	testImplementation("org.mockito:mockito-inline:3.5.13")
 }
