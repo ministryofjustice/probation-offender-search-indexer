1623c1dd6437d08b9be98555796c0057c58ba77a
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-01-11 14:42:23 +0000

    DT-1484 Upgrade to remove lang-tag false positive CVEs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "2.1.2"
   kotlin("plugin.spring") version "1.4.21"
 }
 
@@ -17,14 +17,14 @@ dependencies {
 
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.918")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.934")
 
   implementation("com.google.code.gson:gson:2.8.6")
   implementation("io.arrow-kt:arrow-core:0.11.0")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.1")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.1")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.1")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.2")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.2")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.testcontainers:localstack:1.15.1")
@@ -35,5 +35,5 @@ dependencies {
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.22.0")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
-  testImplementation("org.mockito:mockito-inline:3.6.28")
+  testImplementation("org.mockito:mockito-inline:3.7.0")
 }
