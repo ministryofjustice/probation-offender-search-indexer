28840d412bb59dd1ed1c20911042143f6c6fba53
Mike Halma <mike.halma@digital.justice.gov.uk>
2021-04-01 09:05:50 +0100

    DT-1829 useLatestVersions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.4"
   kotlin("plugin.spring") version "1.4.30"
   id("org.unbroken-dome.test-sets") version "3.0.1"
 }
@@ -22,25 +22,25 @@ dependencies {
 
   implementation("org.springframework:spring-jms")
   implementation("com.amazonaws:amazon-sqs-java-messaging-lib:1.0.8")
-  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.959")
+  implementation("com.amazonaws:aws-java-sdk-elasticsearch:1.11.989")
 
   implementation("com.google.code.gson:gson:2.8.6")
-  implementation("io.arrow-kt:arrow-core:0.11.0")
+  implementation("io.arrow-kt:arrow-core:0.13.0")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.4")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.4")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
 
   implementation("org.awaitility:awaitility-kotlin:4.0.3")
 
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
   testImplementation("org.testcontainers:localstack:1.15.2")
   testImplementation("org.testcontainers:elasticsearch:1.15.2")
-  testImplementation("io.kotest:kotest-assertions-arrow-jvm:4.4.1")
+  testImplementation("io.kotest:kotest-assertions-arrow-jvm:4.4.3")
   testImplementation("org.springframework.security:spring-security-test")
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.24.0")
+  testImplementation("net.javacrumbs.json-unit:json-unit-assertj:2.25.0")
   testImplementation("io.swagger.parser.v3:swagger-parser-v2-converter:2.0.24")
-  testImplementation("org.mockito:mockito-inline:3.7.7")
-  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.4.2")
+  testImplementation("org.mockito:mockito-inline:3.8.0")
+  testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:1.4.3")
 }
