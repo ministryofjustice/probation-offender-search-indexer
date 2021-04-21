46405956d2999f54802200a1b7e1f709692ba69b
Peter Phillips <peter@greenthistle.co.uk>
2021-04-21 13:36:55 +0100

    DT-1949: ⬆️ Update to latest localstack

--- create-offender-changed-messages-local.bash
+++ create-offender-changed-messages-local.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-aws --endpoint-url=http://localhost:4575 sns publish \
+aws --endpoint-url=http://localhost:4566 sns publish \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:probation_offender_events \
     --message-attributes '{"eventType" : { "DataType":"String", "StringValue":"OFFENDER_CHANGED"}}' \
     --message '{"offenderId":11,"crn":"CRN11","nomsNumber":"AN1234Z"}'
--- src/test/resources/localstack/setup-sns.sh
+++ src/test/resources/localstack/setup-sns.sh
@@ -6,18 +6,18 @@ export AWS_SECRET_ACCESS_KEY=foobar
 export AWS_DEFAULT_REGION=eu-west-2
 export PAGER=
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name probation_offender_search_index_dl_queue
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name probation_offender_search_index_queue
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/probation_offender_search_index_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:probation_offender_search_index_dl_queue\"}"}'
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name probation_offender_search_index_dl_queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name probation_offender_search_index_queue
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/probation_offender_search_index_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:probation_offender_search_index_dl_queue\"}"}'
 
-aws --endpoint-url=http://localhost:4575 sns create-topic --name probation_offender_events
+aws --endpoint-url=http://localhost:4566 sns create-topic --name probation_offender_events
 
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name probation_offender_search_event_dl_queue
-aws --endpoint-url=http://localhost:4576 sqs create-queue --queue-name probation_offender_search_event_queue
-aws --endpoint-url=http://localhost:4576 sqs set-queue-attributes --queue-url "http://localhost:4576/queue/probation_offender_search_event_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:probation_offender_search_event_dl_queue\"}"}'
-aws --endpoint-url=http://localhost:4575 sns subscribe \
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name probation_offender_search_event_dl_queue
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name probation_offender_search_event_queue
+aws --endpoint-url=http://localhost:4566 sqs set-queue-attributes --queue-url "http://localhost:4566/queue/probation_offender_search_event_queue" --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:eu-west-2:000000000000:probation_offender_search_event_dl_queue\"}"}'
+aws --endpoint-url=http://localhost:4566 sns subscribe \
     --topic-arn arn:aws:sns:eu-west-2:000000000000:probation_offender_events \
     --protocol sqs \
-    --notification-endpoint http://localhost:4576/queue/probation_offender_search_event_queue \
+    --notification-endpoint http://localhost:4566/queue/probation_offender_search_event_queue \
     --attributes '{"FilterPolicy":"{\"eventType\":[ \"OFFENDER_CHANGED\"] }"}'
 echo All Ready
